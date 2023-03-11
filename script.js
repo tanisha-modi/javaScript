e198f6b38d946017829ef19e5eeece00bf2c48c5
tanisha <tanishamodi12@gmail.com>
2023-03-11 22:34:18 +0530

    js

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert("enter the value of a")
+// let a = prompt("enter the value of a here", "657" /*default value can also be given to prompt*/ )
+// a = parseInt(a)
+// alert("the type of a is " + (typeof a))
+// let write = confirm("do you want to write it to the page")
+// if (write) {
+//      document.write(a)
+//     }
+//     else{
+//     document.write("please allow me to write")
+// }
+
+window.console.log(window)
+console.log(document.body)
+console.log(document.body.style.background = "yellow")
