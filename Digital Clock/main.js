c8bbfcf9a4a2368f930186bdd1fdca195b6f318d
tanisha <tanishamodi12@gmail.com>
2023-07-12 23:26:48 +0530

    digital clock

--- Digital Clock/main.js
+++ Digital Clock/main.js
@@ -1,6 +1,12 @@
 const time = document.getElementById("time");
 
 setInterval(()=>{
-    let d = new Date()
+    let date = new Date()
+    let options  = {
+         weekday : "long", year : "numeric", month : "short", 
+         day : "numeric", hour : "2-digit", minute : "2-digit", second : "2-digit"
+    };
+
+    let d = date.toLocaleTimeString("en-us", options);
     time.innerHTML = d;
 }, 1000)
